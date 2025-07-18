5180c1dc717006bdb53a6d2451dcb9fe981773c9
Joey34kaze <ragnar1724@gmail.com>
2025-07-18 20:32:54 +0200

    Update autoTradeSand with enhanced features: manual trade, debug commands, and improved error handling

--- README.md
+++ README.md
@@ -1,51 +1,44 @@
-# tradeDispenser (Sand Edition)
+# autoTradeSand
 
-tradeDispenser is an addon made for lazy mages. It allows to automatically trade any sand or food you have in your bags.
+A minimal WoW Classic Era addon that automatically trades Hourglass Sand when someone opens a trade window with you.
 
-You can take this configuration as a basic reference (CTRL-click to have a full size view in a new tab) :
+## Features
 
-![tradedispenser_configuration](https://user-images.githubusercontent.com/24671466/26872536-2704e430-4b77-11e7-82f3-3dd6279518f5.png)
-
-This is what every class will get from each trade :
-- Rogues / Warriors: 20 x Conjured Cinnamon Roll
-- Druids / Paladins / Priests : 40 x Hourglass Sand
-- Mages / Hunters : 20 x Hourglass Sand
-- Warlocks : 20 x Hourglass Sand & 20 x Conjured Cinnamon Roll
-
-You can set it up differently if it doesn't suit your needs.
-
-## Screenshot
-
-- Full preview from the Settings Panel
-
-![tradedispenser](https://user-images.githubusercontent.com/24671466/26871282-f6eefc08-4b72-11e7-941f-e6af5f4bf612.png)
-
-- The setting "Display Sand/Food stock" [enable/disable] will allow you to broadcast your stock besides the broadcast message
-
-![tradedispenser_stock](https://user-images.githubusercontent.com/24671466/26871775-987ccbe4-4b74-11e7-85c6-3343253bb652.png)
+- **Simple console commands** - No GUI needed
+- **Automatic sand trading** - Just enable and forget
+- **Configurable amount** - Trade 1 or 2 sand
+- **Lightweight** - Only 2 files, minimal code
 
 ## Installation
-1. Close any instance of WoW
-2. Download **[tradeDispenser Zip file](https://github.com/Linae-Kronos/tradeDispenser/archive/master.zip)**
-3. Extract the zip file wherever you want
-4. Copy the folder "tradeDispenser" to Wow_Folder\Interface\AddOns. Your addon folder should look like this :
-```
-├── WoW_Folder
-    ├── Interface
-        ├── Addons
-            ├── tradeDispenser
-```
 
-5. This is what you should have on character selection screen :
-![tradedispenser_charselect](https://user-images.githubusercontent.com/24671466/26873077-eda4328e-4b78-11e7-8263-102dfae89e37.png)
+1. Download the `autoTradeSand` folder
+2. Copy it to `World of Warcraft\_classic_\Interface\AddOns\`
+3. Enable the addon in-game
 
-## FAQ
+## Usage
 
-1. With this automation, can't it be considered as botting ?
+### Commands
 
-The only thing that can be bannable is if you try to abuse from this addon (for example by doing gold from automatic trades). The addon uses the functions AcceptTrade() & CancelTrade() that could perfectly be used in macros. There is absolutely no reason people can get banned only using this addon : the behaviour is the reason people can get banned.
+- `/ats start` - Enable trading 1 sand
+- `/ats 2` - Enable trading 2 sand  
+- `/ats stop` - Disable trading
+- `/ats status` - Show current status
 
-## Credits
+### How it works
 
-Kaboom the original author
-Sand Edition modifications by Feraline@Spineshatter
+1. Type `/ats start` or `/ats 2` to enable
+2. When someone trades with you, it automatically puts sand in the trade window
+3. That's it!
+
+## Requirements
+
+- WoW Classic Era
+- Hourglass Sand in your bags
+
+## Author
+
+Feraline@Spineshatter
+
+## License
+
+Free to use and modify
--- autoTradeSand/autoTradeSand.lua
+++ autoTradeSand/autoTradeSand.lua
@@ -1,5 +1,6 @@
 -- autoTradeSand - Minimal sand trading addon
 -- Author: Feraline@Spineshatter
+-- Based on working WaterDispenser pattern
 
 local ATS_Config = ATS_Config or {
     enabled = false,
@@ -10,81 +11,72 @@ local frame = CreateFrame("Frame")
 frame:RegisterEvent("TRADE_SHOW")
 frame:RegisterEvent("VARIABLES_LOADED")
 
-local tradeState = nil
-local tradeData = {}
-
 frame:SetScript("OnEvent", function(self, event, ...)
     if event == "VARIABLES_LOADED" then
         print("|cFF00FF00autoTradeSand loaded! Use /ats start or /ats 2|r")
     elseif event == "TRADE_SHOW" and ATS_Config.enabled then
         print("|cFFFFFF00Trade window opened - autoTradeSand triggered|r")
-        -- Reset trade state
-        tradeState = "populate"
-        tradeData.slotID = 1
-        tradeData.numAttempts = 0
-        tradeData.containerLocation = nil
-        
-        -- Start the trade process
-        C_Timer.After(0.1, function()
-            TradeSand()
+        -- Simple delay then trade
+        C_Timer.After(0.2, function()
+            SimpleTradeSand()
         end)
     end
 end)
 
-function TradeSand()
-    if not ATS_Config.enabled or tradeState ~= "populate" then 
-        print("|cFFFF0000TradeSand: Not enabled or wrong state|r")
+function SimpleTradeSand()
+    if not ATS_Config.enabled then 
+        print("|cFFFF0000SimpleTradeSand: Not enabled|r")
         return 
     end
     
-    print("|cFFFFFF00Searching for Hourglass Sand in bags...|r")
+    print("|cFFFFFF00Searching for Hourglass Sand...|r")
     
     -- Find Hourglass Sand in bags
     for bag = 0, 4 do
         for slot = 1, GetContainerNumSlots(bag) do
             local itemLink = GetContainerItemLink(bag, slot)
-            if itemLink then
-                print("|cFFFFFF00Found item in bag " .. bag .. " slot " .. slot .. ": " .. itemLink .. "|r")
-                if string.find(itemLink, "Hourglass Sand") then
-                    print("|cFF00FF00Found Hourglass Sand!|r")
-                    local _, itemCount = GetContainerItemInfo(bag, slot)
-                    print("|cFFFFFF00Item count: " .. itemCount .. "|r")
-                    if itemCount >= ATS_Config.sandCount then
-                        print("|cFF00FF00Attempting to pick up " .. ATS_Config.sandCount .. " sand...|r")
-                        -- Pick up the sand
-                        PickupContainerItem(bag, slot)
-                        
-                        -- Wait a frame to ensure the item is on cursor
-                        C_Timer.After(0.05, function()
-                            if CursorHasItem() then
-                                print("|cFF00FF00Item on cursor, clicking trade button...|r")
-                                -- Put it in the first trade slot
-                                ClickTradeButton(1)
-                                print("|cFF00FF00Traded " .. ATS_Config.sandCount .. " Hourglass Sand|r")
-                                tradeState = nil
-                            else
-                                print("|cFFFF0000Item not on cursor, retrying...|r")
-                                -- Try again if item didn't get picked up
-                                tradeData.numAttempts = tradeData.numAttempts + 1
-                                if tradeData.numAttempts < 10 then
-                                    C_Timer.After(0.1, TradeSand)
-                                else
-                                    print("|cFFFF0000Failed to pick up Hourglass Sand after 10 attempts|r")
-                                    tradeState = nil
-                                end
-                            end
-                        end)
-                        return
-                    else
-                        print("|cFFFF0000Not enough sand (need " .. ATS_Config.sandCount .. ", have " .. itemCount .. ")|r")
-                    end
+            if itemLink and string.find(itemLink, "Hourglass Sand") then
+                print("|cFF00FF00Found Hourglass Sand in bag " .. bag .. " slot " .. slot .. "|r")
+                local _, itemCount = GetContainerItemInfo(bag, slot)
+                print("|cFFFFFF00Item count: " .. itemCount .. "|r")
+                
+                if itemCount >= ATS_Config.sandCount then
+                    print("|cFF00FF00Attempting to trade " .. ATS_Config.sandCount .. " sand...|r")
+                    
+                    -- Simple approach: just pick up and click
+                    PickupContainerItem(bag, slot)
+                    
+                    -- Wait a moment then click trade button
+                    C_Timer.After(0.1, function()
+                        if CursorHasItem() then
+                            print("|cFF00FF00Item on cursor, clicking trade button...|r")
+                            ClickTradeButton(1)
+                            print("|cFF00FF00Successfully traded " .. ATS_Config.sandCount .. " Hourglass Sand|r")
+                        else
+                            print("|cFFFF0000Item not on cursor, trying again...|r")
+                            -- Try one more time
+                            C_Timer.After(0.1, function()
+                                PickupContainerItem(bag, slot)
+                                C_Timer.After(0.1, function()
+                                    if CursorHasItem() then
+                                        ClickTradeButton(1)
+                                        print("|cFF00FF00Successfully traded " .. ATS_Config.sandCount .. " Hourglass Sand (retry)|r")
+                                    else
+                                        print("|cFFFF0000Failed to pick up Hourglass Sand|r")
+                                    end
+                                end)
+                            end)
+                        end
+                    end)
+                    return
+                else
+                    print("|cFFFF0000Not enough sand (need " .. ATS_Config.sandCount .. ", have " .. itemCount .. ")|r")
                 end
             end
         end
     end
     
     print("|cFFFF0000No Hourglass Sand found in bags!|r")
-    tradeState = nil
 end
 
 -- Manual trade function
@@ -108,11 +100,10 @@ function ManualTradeSand()
                     -- Pick up the sand
                     PickupContainerItem(bag, slot)
                     
-                    -- Wait a frame to ensure the item is on cursor
-                    C_Timer.After(0.05, function()
+                    -- Wait a moment then click trade button
+                    C_Timer.After(0.1, function()
                         if CursorHasItem() then
                             print("|cFF00FF00Manual trade: Item on cursor, clicking trade button...|r")
-                            -- Put it in the first trade slot
                             ClickTradeButton(1)
                             print("|cFF00FF00Manually traded " .. ATS_Config.sandCount .. " Hourglass Sand|r")
                         else
@@ -147,7 +138,6 @@ SlashCmdList["AUTOTRADESAND"] = function(msg)
         print("|cFF00FF00autoTradeSand enabled! Will trade 2 sand.|r")
     elseif command == "stop" or command == "off" then
         ATS_Config.enabled = false
-        tradeState = nil
         print("|cFFFF0000autoTradeSand disabled!|r")
     elseif command == "trade" then
         -- Manual trade command - works even if auto is disabled
